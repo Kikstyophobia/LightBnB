47f1afacacf50c5d0edd734a5a6b0fbd82e679bb
Calvin Chung <calvin-chung@live.com>
2022-04-06 17:51:10 -0700

    adding seeds with filler data

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,20 @@
+
+INSERT INTO users (name, email, password)
+VALUES ('Calvin Chung', 'email@email.com', "$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u."),
+('Kalci Do', 'email@email.com', "$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u."),
+('Jesse Chung', 'email@email.com', "$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.");
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (1, 'Mayfair', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 300, 12, 10, 14, 'Canada', '5460 No.7 Rd', 'Richmond', 'British Columbia', '28142', TRUE),
+VALUES (2, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 700, 5, 9, 10, 'Canada', '44 Lewis rd', 'Surrey', 'British Columbia', '63456', TRUE),
+VALUES (3, 'House House', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 800, 4, 8, 11, 'Canada', '808 Hamilton Rd', 'Toronto', 'Ontario', '67896', TRUE);
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 1, 1),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 3, 3);
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (1, 1, 1, 5, 'message'),
+(2, 5, 10, 3, 'message'),
+(3, 4, 1, 4, 'message');
