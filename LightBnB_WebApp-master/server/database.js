7c08e22312ee540a0db9f8fff4f0c48a756df26f
Calvin Chung <calvin-chung@live.com>
2022-04-07 15:26:47 -0700

    revised getAllReservations function with psql query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -72,11 +72,11 @@ const addUser =  function(user) {
   INSERT INTO users (name, email, password)
   VALUES ($1, $2, $3) RETURNING *`, [user.name, user.email, user.password]) 
   .then((result) => {
-    console.log(result.rows[0]);
+    //console.log(result.rows[0]);
     return result.rows[0];
   })
   .catch((err) => {
-    console.log(err);
+    //console.log(err);
   });
 
 };
@@ -90,8 +90,26 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  
+  return pool
+  .query(`
+    SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, limit])
+  .then((result) => {
+    console.log(result.rows);
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
@@ -111,7 +129,7 @@ exports.getAllReservations = getAllReservations;
       return result.rows;
     })
     .catch((err) => {
-       console.log(err.message);
+      console.log(err.message);
     });
 
 };
